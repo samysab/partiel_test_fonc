16b513add0a0c5d077c7f69dfc9fb4780587aef4
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2022-04-04 13:28:22 +0200

    test

--- tests/TrainTest.php
+++ tests/TrainTest.php
@@ -7,10 +7,39 @@ class TrainTest extends PantherTestCase
 {
     public function testMyApp(): void
     {
-        $client = static::createPantherClient(['external_base_uri' => 'http://localhost:8081']);
+        $client = static::createPantherClient(['external_base_uri' => 'http://localhost:8089']);
         $client->request('GET', '/admin/train');
-        sleep(5);
+        sleep(1);
+        //$client->takeScreenshot("screen.png");
+
+        /*
+         * Création d'un train
+         */
         $client->executeScript("document.querySelector('#nouveau-train').click()");
-        sleep(55);
+        $client->executeScript("document.querySelector('#train_name').value = 'testr'");
+        $client->executeScript("document.querySelector('#train_description').value = 'testfsdfsdr'");
+        //  Wait qui ne passe pas
+        //$client->waitForAttributeToContain("#train_name", "value" ,"testezdgdfgdfr");
+        $client->waitForAttributeToContain("#train_name", "value" ,"testr");
+        $client->waitForAttributeToContain("#train_description", "value" ,"testfsdfsdr");
+        sleep(2);
+        $client->executeScript("document.querySelector('#form-train').click()");
+        $this->assertSelectorIsVisible(".toast");
+        sleep(5);
+
+
+        /*
+         * Modification d'un train
+         */
+        $client->executeScript("document.querySelector('#btnModifier_11').click()");
+        sleep(2);
+        $client->executeScript("document.querySelector('#train_name').value = 'modification du nom'");
+        $client->executeScript("document.querySelector('#train_description').value = 'Description'");
+        sleep(2);
+        $client->waitForAttributeToContain("#train_name", "value" ,"modification du nom");
+        $client->waitForAttributeToContain("#train_description", "value" ,"Description");
+        $client->executeScript("document.querySelector('#form-train').click()");
+
+
     }
 }
